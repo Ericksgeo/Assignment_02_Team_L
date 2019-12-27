b068f66fca7c48ae1d865f34730d94080867fe8f
Ja5mineLu <ucesjlu@ucl.ac.uk>
2019-12-27 21:54:19 +0000

    Task 01,02,03,04,06 Finished Task 01,02,03 improved Task 04 added

--- FLOOD_EMERGENCY_PLANNING.py
+++ FLOOD_EMERGENCY_PLANNING.py
@@ -9,6 +9,7 @@ import shapefile
 import numpy as np
 import rasterio
 from rtree import index
+import networkx as nx
 
 
 # creating UI to input coordinates
@@ -85,8 +86,9 @@ class MyWindow:
                         self.n = y
                         break
                     else:
-                        self.lbl4.configure(text="Error: Please Enter Coordinates in range\n (425000,75000)-(470000,"
-                                                 "100000)\n or a postcode of the Isle of Wight")
+                        self.lbl4.configure(
+                            text="Error: Please Enter Coordinates in range\n (425000,75000)-(470000,"
+                                 "100000)\n or a postcode of the Isle of Wight")
                         return
         elif es == "" or nt == "":
             self.lbl4.configure(text="Incomplete Coordinates,\n try again...(Press Reset to clear)")
@@ -117,17 +119,17 @@ class ReadIslandFromShp:
         return island
 
 
-class ReadRoadsFromShp:
+class ReadNodesFromShp:
     def __init__(self, roadsfilepath):
         self.__filepath = roadsfilepath
 
     def get_nodes(self):
         nodes = shapefile.Reader(self.__filepath)
         shapes = nodes.shapes()
-        pointslist = []
+        points = []
         for i in range(len(shapes)):
-            pointslist.append(tuple(shapes[i].points[0]))
-        return pointslist
+            points.append(tuple(shapes[i].points[0]))
+        return points
 
     def get_point_attribute(self):
         nodes = shapefile.Reader(self.__filepath)
@@ -143,7 +145,7 @@ class PointCheck:
     def test_point(self):
         # test whether input point is on the island
         if self.__point.touches(self.__polygon) or self.__point.within(self.__polygon):
-            status = "Available Location. Calculating..."
+            status = "Available Location. Ready for calculation."
             print(status)
         else:
             status = "Error, point outside island boundaries."
@@ -156,11 +158,11 @@ class PointCheck:
 
 class Elevation:
 
-    def __init__(self, elefilepath, point):
-        self.__point = point
+    def __init__(self, elefilepath):
         self.__filepath = elefilepath
         with open(self.__filepath, "r") as elefile:
             self.__lines = elefile.readlines()
+        # init elevation data array
 
     def get_ncols(self):
         line0 = self.__lines[0].strip().split(" ")
@@ -182,8 +184,10 @@ class Elevation:
         line4 = self.__lines[4].strip().split(" ")
         return int(line4[1])
 
+    def elevation_data_info(self):
+        return self.get_ncols(), self.get_nrows(), self.get_xllcorner(), self.get_yllcorner(), self.get_cellsize()
+
     def get_elevation(self):
-        # init elevation data array
         self.__eledata = np.zeros((self.get_nrows(), self.get_ncols()))
         row = 0
         # read elevation data
@@ -194,7 +198,8 @@ class Elevation:
             row = row + 1
         return self.__eledata
 
-    def get_max_point(self, area):
+    def get_max_point(self, area, point):
+        self.__point = point
         features = [area.__geo_interface__]
         # use shp to clip the rasterio
         # Source: https://www.cnblogs.com/shoufengwei/p/6437934.html
@@ -203,7 +208,7 @@ class Elevation:
             area_ele = out_image.reshape(out_image.shape[1], out_image.shape[2])
         a = np.where(area_ele == np.max(area_ele))
         shortest = 10000
-        # get the nearst point in Euclidean distance as the only highest point
+        # get the nearest point in Euclidean distance as the only highest point
         for m in range(0, len(a[0])):
             xr = a[0][m]
             yr = a[1][m]
@@ -221,12 +226,14 @@ class Elevation:
 
 class NearestITN:
     def __init__(self, user_point, safe_point, road_point):
-        self.__user_point = (user_point.x,user_point.y)
-        self.__safe_point = (safe_point.x,safe_point.y)
+        self.__user_point = (user_point.x, user_point.y)
+        self.__safe_point = (safe_point.x, safe_point.y)
         self.__road_point = road_point
 
     def Rtree(self, attribute):
         idx = index.Index()
+        index1 = 0
+        index2 = 0
         for n, point in enumerate(self.__road_point):
             idx.insert(n, point, str(n))
 
@@ -242,11 +249,75 @@ class NearestITN:
         return user_point_name, safe_point_name
 
 
+class ShortestPath:
+
+    def __init__(self, itn_filepath, ele_array, ele_info):
+        self.__filepath = itn_filepath
+        self.__ele_array = ele_array
+        self.__ele_ncols = ele_info[0]
+        self.__ele_nrows = ele_info[1]
+        self.__ele_xllcorner = ele_info[2]
+        self.__ele_yllcorner = ele_info[3]
+        self.__ele_cell = ele_info[4]
+
+    def get_path(self, start, end):
+        self.__start = start
+        self.__end = end
+        # create the graph from the dictionary loaded from the JSON file
+        solent_itn_json = self.__filepath
+        with open(solent_itn_json, "r") as f:
+            solent_itn = json.load(f)
+        g = nx.DiGraph()
+        road_links = solent_itn['roadlinks']
+        road_nodes = solent_itn['roadnodes']
+
+        for link in road_links:
+            point_a_id = road_links[link]['start']
+            point_b_id = road_links[link]['end']
+            # get coordinates of start point and end point of each road
+            for node in road_nodes:
+                if node == point_a_id:
+                    point_a_x = road_nodes[node]['coords'][0]
+                    point_a_y = road_nodes[node]['coords'][1]
+                elif node == point_b_id:
+                    point_b_x = road_nodes[node]['coords'][0]
+                    point_b_y = road_nodes[node]['coords'][1]
+
+            # get the start point raster and end point raster
+            point_a_r = (self.__ele_nrows - (point_a_y - self.__ele_yllcorner) // self.__ele_cell - 1,
+                         (point_a_x - self.__ele_xllcorner) // self.__ele_cell)
+            point_b_r = (self.__ele_nrows - (point_b_y - self.__ele_yllcorner) // self.__ele_cell - 1,
+                         (point_b_x - self.__ele_xllcorner) // self.__ele_cell)
+
+            # get the elevation of start point and end point
+            point_a_ele = self.__ele_array[int(point_a_r[0]), int(point_a_r[1])]
+            point_b_ele = self.__ele_array[int(point_b_r[0]), int(point_b_r[1])]
+
+            # Naismith's rule
+            distance = road_links[link]['length']
+            time1 = distance / 5000 * 60
+            height = abs(point_a_ele - point_b_ele)
+            time2 = height / 10
+            time = time1 + time2
+
+            # add weighed edges
+            if point_a_ele >= point_b_ele:
+                g.add_edge(point_a_id, point_b_id, weight=time1)
+                g.add_edge(point_b_id, point_a_id, weight=time)
+            else:
+                g.add_edge(point_a_id, point_b_id, weight=time)
+                g.add_edge(point_b_id, point_a_id, weight=time1)
+
+        # calculate shortest path
+        path = nx.dijkstra_path(g, source=self.__start, target=self.__end)
+        return path
+
+
 def main():
     # creating window using the MyWindow class
     window = MyWindow()
     window.root.title('FLOOD EMERGENCY PLANNING')
-    window.root.geometry("450x300+100+100")
+    window.root.geometry("450x300+500+200")
     window.root.mainloop()
 
     # test whether input point is in the island boundaries
@@ -255,16 +326,28 @@ def main():
     PointCheck(island, user_point).test_point()
 
     # use island shp to clip the buffer and get the highest point of available area
+    print("Loading elevation dataset...")
+    elevation_data = Elevation("Material/elevation/SZ.asc").get_elevation()
+    elevation_data_info = Elevation("Material/elevation/SZ.asc").elevation_data_info()
+
+    print("Searching for highest point within 5km...")
     shparea = user_point.buffer(5000).intersection(island)
-    highest_point = Elevation("Material/elevation/SZ.asc", user_point).get_max_point(shparea)
+    highest_point = Elevation("Material/elevation/SZ.asc").get_max_point(shparea, user_point)
     print("The highest point within 5km is:", highest_point)
 
     # find nearest point
-    road_point = ReadRoadsFromShp(r'Material\roads\nodes.shp').get_nodes()
-    attribute = ReadRoadsFromShp(r'Material\roads\nodes.shp').get_point_attribute()
+    print("Searching for available path...")
+    road_point = ReadNodesFromShp("Material/roads/nodes.shp").get_nodes()
+    attribute = ReadNodesFromShp("Material/roads/nodes.shp").get_point_attribute()
+    user_point_id, safe_point_id = NearestITN(user_point, highest_point, road_point).Rtree(attribute)
+    print("Path Start Point ID:", user_point_id, "\nPath End Point ID:", safe_point_id)
 
-    user_point_name, safe_point_name = NearestITN(user_point, highest_point, road_point).Rtree(attribute)
-    print("Path Start Point ID:",user_point_name, "\nPath End Point ID:",safe_point_name)
+    print("Calculating the shortest path...")
+    path = ShortestPath("Material/itn/solent_itn.json", elevation_data, elevation_data_info).get_path(user_point_id,
+                                                                                                      safe_point_id)
+    # print(path)
+    print("Path created.")
+
 
 if __name__ == "__main__":
     main()
